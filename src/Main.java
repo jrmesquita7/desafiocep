8b62397416b43701719a6d229ec39d0a4bf3ab6f
jrmesquita7 <cleobetojunior1999@gmail.com>
2024-10-10 16:44:29 -0300

    feature

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -12,12 +13,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite seu CEP");
+        var cep = leitura.nextLine();
 
         ConsultaCep consultaCep = new ConsultaCep();
         try{
-            Endereco endereco = consultaCep.buscaEndereco("65045380");
+            Endereco endereco = consultaCep.buscaEndereco(cep);
             System.out.println(endereco);
-        }catch (RuntimeException e){
+            GeradorArquivo geradorArquivo = new GeradorArquivo();
+            geradorArquivo.salvaJson(endereco);
+        }catch (RuntimeException | IOException e){
             System.out.println(e.getMessage());
         }
 
